698c12bf68e5b1194f58c1c4540d8d9aabbbdaca
soongjamm <kimstz0303@gmail.com>
2022-02-14 20:58:10 +0900

    step3 with when

--- src/main/kotlin/com/coursera/fromjavatokotlin/Color.kt
+++ src/main/kotlin/com/coursera/fromjavatokotlin/Color.kt
@@ -11,12 +11,10 @@ fun main() {
 
 private fun hello(degrees: Int) {
     val (description: String, color: Color) =
-            if (degrees < 10) {
-                Pair("cold", Color.BLUE)
-            } else if (degrees < 25) {
-                Pair("mild", Color.ORANGE)
-            } else {
-                Pair("hot", Color.RED)
+            when {
+                degrees < 10 -> Pair("cold", Color.BLUE)
+                degrees < 25 -> Pair("mild", Color.ORANGE)
+                else -> Pair("hot", Color.RED)
             }
 
     println("${description}, ${color}")
